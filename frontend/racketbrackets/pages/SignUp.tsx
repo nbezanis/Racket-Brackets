88cad460e19750b1dd0dd6c7741096814b0c9151
jbsandling <jbsandling@gmail.com>
2021-07-18 23:16:13 -0400

    Added Login Page

--- frontend/racketbrackets/pages/SignUp.tsx
+++ frontend/racketbrackets/pages/SignUp.tsx
@@ -5,17 +5,18 @@ import firebase from 'firebase';
 import Head from 'next/head';
 import { AuthContext } from '../auth/AuthContext';
 import { auth } from '../config/firebaseConfig';
+import { useRouter } from 'next/dist/client/router';
 
 
 const SignUp = () => {
     const user = useContext(AuthContext);
 
+    const router = useRouter();
+
     const emailRef = useRef<HTMLInputElement>(null);
     const usernameRef = useRef<HTMLInputElement>(null);
     const passwordRef = useRef<HTMLInputElement>(null);
     const db = firebase.database()
-    //console.log(db.ref('users').once('username'))
-
 
     const validate = () => {
       //TO DO:
@@ -27,10 +28,8 @@ const SignUp = () => {
     const CreateUser = async () => {
       try{
         await auth.createUserWithEmailAndPassword(emailRef.current!.value,passwordRef.current!.value)
-        .then((userCredential) => {
-          console.log("user!");
-          // const uid = user.uid;
-          // alert(uid);
+        .then(() => {
+          //console.log("user!");
           const uname = usernameRef.current!.value;
           var userRef = db.ref('users');
           userRef.child(uname).set({username: uname, 
@@ -42,6 +41,8 @@ const SignUp = () => {
                                     upcomingMatches: false,
                                     groups: false});
         });
+        //Replace this push with a push to the correct profile page
+        router.push("/");
       } catch(e) {
         console.error(e);
       }
@@ -58,7 +59,7 @@ const SignUp = () => {
             <input type="text" id="email" name="email" placeholder="Email" ref={emailRef}/><br/>
             <input type="password" id="password" name="password" placeholder="Password" ref={passwordRef}/><br/>
             <input type="password" id="confPassword" name="confPassword" placeholder="Confirm Password"/><br/>
-            <button type="button" onClick={CreateUser}>SignUp</button>
+            <button type="button" onClick={CreateUser}>Sign Up</button>
       </form>
     </div>
     );
