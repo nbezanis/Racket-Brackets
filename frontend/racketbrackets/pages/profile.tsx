1a037964663734d2fc73cb7632960a13c8e4ecd6
Caleb <esquinoman@gmail.com>
2021-07-22 17:03:12 -0400

    fixed profile.tsx

--- frontend/racketbrackets/pages/profile.tsx
+++ frontend/racketbrackets/pages/profile.tsx
@@ -1,15 +1,47 @@
-import Head from 'next/head';
-
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import profilePic from './images/default.png'
+import styles from '../styles/profile.module.css'
+ 
+const name = 'Temp'
+const rating = '1400'
+ 
 const Profile = () => {
-  //Should be replaced by Profile Page
     return (
-        <div>
-            <Head>
-                <title>Profile Page</title>
-            </Head>
-            <h1>Profile Page</h1>
+      <div className={styles.container}>
+      <main className={styles.main}>     
+ 
+        <div className = {styles.imgwithtext}>
+            <Image
+              priority
+              src={profilePic}
+              className={styles.borderCircle}
+              height={288}
+              width={288}
+              alt={name}
+            />
+            <p>Username: {name}</p>
+            <p>Rating: {rating}</p>
         </div>
+ 
+        <h2>Match History</h2>
+        <ul>
+          <li>Profile 1 vs Profile 2 <u>WIN</u></li>
+          <li>Profile 1 vs Profile 3 <u>WIN</u></li>
+          <li>Profile 1 vs Profile 4 <u>LOSS</u></li>
+        </ul>
+        <h2>Active Communities</h2>
+        <ul>
+          <li><u>RPI Tennis Club</u></li>
+          <li><u>Troy Tennis Club</u></li>
+        </ul>
+      </main>
+ 
+    </div>
+ 
     );
 }
-
+ 
 export default Profile;
+
