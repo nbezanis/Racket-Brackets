4a0a1c47adabe041af19b4cd225e8a3fb9be4525
nbezanis <nbezanis98@gmail.com>
2021-08-03 17:24:47 -0400

    extract data from input

--- frontend/racketbrackets/pages/JoinGroup.tsx
+++ frontend/racketbrackets/pages/JoinGroup.tsx
@@ -1,20 +1,53 @@
-import {useRef} from "react";
+import {Component, useRef} from "react";
+import {Community} from "../Classes/Community";
 
-const JoinGroup = () => {
-    const groupNameRef = useRef<HTMLInputElement>(null);
+export default class JoinGroup extends Component {
 
+    groupName: string = "";
+    errorMessage: string = "";
 
+    state = {
+        displayError: false,
+    };
 
-    return(
-        <div>
-            <head><title>Join Group</title></head>
-            <p>Request to join a group:</p>
-            <form method="post">
-                <input type="text" name="Group Name"/>
-                <button>Request to Join</button>
-            </form>
-        </div>
-    );
+    getErrorDisplay = () => {
+        if(this.state.displayError){
+            return(
+                <p>{this.errorMessage}</p>
+            );
+        }else{
+            return(
+                <div></div>
+            );
+        }
+    };
+
+    onInput = (e: any) => {
+        this.groupName = e.target.value;
+    };
+
+    onSubmit = () => this.search().then(this.requestAccess);
+
+    search = async () => {
+
+    };
+
+    requestAccess = () => {
+
+    }
+
+    render() {
+        return(
+            <div>
+                <head><title>Join Group</title></head>
+                <p>Request to join a group:</p>
+                <form method="post">
+                    <input type="text" name="Group Name" onInput={this.onInput}/>
+                    <button onClick={this.onSubmit}>Request to Join</button>
+                </form>
+                {this.getErrorDisplay()}
+
+            </div>
+        );
+    }
 };
-
-export default  JoinGroup;
