9a07030857a65414af4ee93fa435ed47e7d16ee0
Carl Stadtler <carl.stadtler@gmail.com>
2021-08-03 16:46:37 -0400

    community search correctly redirects to profile page

--- frontend/racketbrackets/pages/index.tsx
+++ frontend/racketbrackets/pages/index.tsx
@@ -2,8 +2,11 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css'
 import * as React from "react";
 
+/*
+*This is the home page of the app. The two main parts of the home page are search bars that
+*allow a user to search for other users or search for communities. s
+*/
 const Home = () => {
-  //Should be replaced by Home Page
   return (
     <main>
       <div className = {styles.main}>
@@ -15,8 +18,8 @@ const Home = () => {
           </form>
         </a> 
         <a className = {styles.searchCard}>
-          <form action="/search" method="get">
-            <input type="text" placeholder="Search for Communities" id="query" name="query" />
+          <form action="/GroupProfile" method="get">
+            <input type="text" placeholder="Search for Communities" id="name" name="name" />
             <button type="submit">Search</button>
           </form>
         </a>    
