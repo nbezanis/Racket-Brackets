aee9d55c27bb9abe7f88d06fb89d51ab9e442c12
jbsandling <jbsandling@gmail.com>
2021-08-06 16:52:27 -0400

    Added more documentation to classes

--- frontend/racketbrackets/Classes/Challenge.ts
+++ frontend/racketbrackets/Classes/Challenge.ts
@@ -11,6 +11,14 @@ import firebase from "firebase"
 */
 export class Challenge extends Post{
     match: Match = new Match(0, firebase.database());
+
+    //Constructor for Challenge Class
+    constructor(from: string, time: Date, loc:string, m: Match){
+        super(from,"Match Challenge","Challenge from " + from + " at " + time + ", " + loc);
+        this.match = m;
+
+    }
+    //Challenge.acceptChallenge()
     acceptChallenge(){
         //Should add the accepting user to the array of users in the Match
     }
