d288c71458d42673ba82c940c1fcff7e5c2c966a
jbsandling <jbsandling@gmail.com>
2021-08-11 12:45:16 -0400

    Added basic match scoring

--- frontend/racketbrackets/pages/MatchDetails.tsx
+++ frontend/racketbrackets/pages/MatchDetails.tsx
@@ -1,9 +1,96 @@
-import React, { Component } from "react";
+import React, { Component, useRef, useState } from "react";
 import { Match } from "../Classes/Match";
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import firebase from "firebase";
 import styles from "../styles/MyMatches.module.css"
+import { User } from "../Classes/User";
+
+interface ScoreProps{
+    p1:string,
+    p2:string
+}
+
+class Score extends Component<ScoreProps>{
+
+    private p1Ref = React.createRef<HTMLInputElement>()
+    private p2Ref = React.createRef<HTMLInputElement>()
+
+    state = {
+        player1: " ",
+        player2: " ",
+        user1: new User(" ",firebase.database()),
+        user2: new User(" ",firebase.database())
+    }
+
+    constructor(props:any) {
+        super(props);
+        this.setState({
+            player1: this.props.p1,
+            player2: this.props.p2
+        });
+    }
+
+    async componentDidMount() {
+        await this.loadUser();
+    }
+
+    loadUser = async() => {
+        const db = firebase.database();
+        var userRef = db.ref("/users").once("value")
+            .then(snapshot => {
+                const p1 = snapshot.child(this.props.p1).val();
+                const p2 = snapshot.child(this.props.p2).val();
+                this.setState({
+                    user1: p1,
+                    user2: p2
+                });
+            })
+        
+    }
+
+    async score() {
+        var p1Rating = this.state.user1.rating;
+        var p2Rating = this.state.user2.rating;
+        console.log(p1Rating + " " + p2Rating)
+        var p1Prob = (1.0/(1+Math.pow(((p2Rating - p1Rating)/400),10)));
+        var p2Prob = (1.0/(1+Math.pow(((p1Rating - p2Rating)/400),10)));
+        console.log(p1Prob + " " + p2Prob)
+        if(this.p1Ref.current!.checked) {
+            console.log("p1");
+            p1Rating += 30*(1-p1Prob);
+            p2Rating += 30*(0-p2Prob);
+        } else if(this.p2Ref.current!.checked){
+            console.log("p2");
+            p1Rating += 30*(0-p1Prob);
+            p2Rating += 30*(1-p2Prob);
+        }
+        console.log("P1: " + p1Rating + " P2: " + p2Rating);
+        var userRef = firebase.database().ref('users');
+            userRef.child(this.state.user1.username).update({
+                rating: p1Rating
+            });
+            userRef.child(this.state.user2.username).update({
+                rating: p2Rating
+            });
+    }
+
+    render() {
+        return(
+            <div>
+            
+            <b> Choose Winner</b>
+            <form method="post">
+                <input type="radio" id="p1" name="winner" value={this.props.p1} ref={this.p1Ref}/>
+                <label htmlFor="p1">{this.props.p1}</label><br/>
+                <input type="radio" id="p2" name="winner" value={this.props.p2} ref={this.p2Ref}/>
+                <label htmlFor="p2">{this.props.p2}</label><br/>
+                <button type="button" onClick={() => this.score()}> Score Game </button>
+            </form>
+            </div>
+        )
+    }
+}
 
 interface MDProps {
     id: string
@@ -17,7 +104,8 @@ class MatchData extends Component<MDProps>{
         loading: true,
         details: new Match(0,firebase.database().ref("matches")),
         past: false,
-        opponent: " "
+        opponent: " ",
+        user: " "
     }
 
     constructor(props:any) {
@@ -42,6 +130,8 @@ class MatchData extends Component<MDProps>{
                 players.forEach((p:string) => {
                     if(p != localStorage.getItem("username")) {
                         this.setState({opponent: p});
+                    } else {
+                        this.setState({user: p});
                     }
                 });
                 const d: Date = new Date(match.date);
@@ -69,7 +159,7 @@ class MatchData extends Component<MDProps>{
                 <p><b>Match Date:</b> {this.formatDates(this.state.details.date)}</p>
                 <p><b>Match Location:</b> {this.state.details.location}</p>
                 <p><b>Opponent: </b><a href={`/Profile/?name=${this.state.opponent}`}>{this.state.opponent}</a></p>
-                {(!(this.state.details.score) && (this.state.past))? <p>scoreButton</p> : null}
+                {(!(this.state.details.score) && (this.state.past))? <Score p1={this.state.user} p2={this.state.opponent}/> : null}
             </div>
         )
     }
